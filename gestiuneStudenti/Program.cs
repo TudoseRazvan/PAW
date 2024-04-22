77f344a54110b839c410ca9c4cc33f3be295a733
Tudose Razvan <146849110+TudoseRazvan@users.noreply.github.com>
2024-04-22 20:07:49 +0300

    Add files via upload

---
+++ gestiuneStudenti/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace gestiuneStudenti
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            Form1 form1 = new Form1();  // Inițializează Form1
+            FormStudent formStudent = new FormStudent(form1);  // Inițializează FormStudent cu referința către Form1
+
+            form1.Show();  // Afișează Form1
+            Application.Run();  // Rulează aplicația
+        }
+    }
+}
---
+++ gestiuneStudenti/Student.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace gestiuneStudenti
+{
+    public class Student : Persoana
+    {
+        private string facultate;
+        private int[] note;
+
+        public Student(int marca, string nume, string facultate, int[] note) : base(marca, nume)
+        {
+            this.facultate = facultate;
+            this.note = note;
+        }
+
+        public string Facultate
+        { 
+        get { return this.facultate;}
+        set { this.facultate = value; }
+        }
+
+        public int[] Note
+        { 
+            get { return this.note;}
+            set { this.note = value; }
+        }
+
+        //Metoda pentru a calcula media
+        public static implicit operator float(Student student)
+        {
+            if(student.note == null || student.note.Length == 0)
+            {
+                return 0;
+            }
+
+            int sum = 0;
+            foreach(int nota in student.note) 
+            {
+                sum += nota;
+            }
+            return (float)sum / student.note.Length;
+        }
+
+        public override string ToString()
+        {
+            return $"Marca: {Marca}, Nume: {Nume}, Facultate: {facultate}, Note: [{string.Join(", ", note)}]";
+        }
+
+    }
+}
