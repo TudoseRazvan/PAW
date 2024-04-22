0986d3deadf494d58059b19a2b6be1b052dc9789
Tudose Razvan <146849110+TudoseRazvan@users.noreply.github.com>
2024-04-22 23:26:24 +0300

    Add files via upload

---
+++ ReferinteBibliograficeFinal/Autor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReferinteBibliograficeFinal
+{
+    public class Autor
+    {
+        private string nume;
+        private string grad_didactic;
+        private const int marca = 1;
+
+
+        public Autor(string nume, string grad_didactic) 
+        {
+            this.nume = nume;
+            this.grad_didactic = grad_didactic;
+        }
+
+        public string Nume
+        {
+            get { return nume; }
+            set { nume = value; }
+        }
+
+        public string Grad_Didactic
+        {
+            get { return grad_didactic; }
+            set { grad_didactic = value; }
+        }
+
+
+    }
+}
